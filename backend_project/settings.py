fa80d9c9f8767366620e4007a39b25e7dff10f9c
JoelUriarte14 <joeldavila141516@gmail.com>
2025-11-13 19:05:18 -0500

    Actualziacion de CSRF

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -27,7 +27,16 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'api.conocemoyobamba.com',
+    'localhost',
+    '127.0.0.1'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://api.conocemoyobamba.com',
+    'https://conocemoyobamba.com',
+]
 
 
 # Application definition
